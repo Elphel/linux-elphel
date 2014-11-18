b6ff4022a0f568514de04c50414d0a4d8242bd6b
Andrey Filippov <andrey@elphel.com>
2014-11-17 17:41:10 -0700

    script to run bitbake from Eclipse

---
+++ run_bitbake.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+args="$@"
+while (( "$#" )); do
+  shift
+done
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo "Launching bitbake $args"
+cd $DIR/..
+. ./oe-init-build-env
+bitbake $args  | sed -u 's@| @@'
+exit 0
