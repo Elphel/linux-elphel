1eb0761870ab3d2fd9a3541b6f8de857155fd8a4
Mikhail Karpenko <mikhail@elphel.com>
2016-04-18 21:24:44 -0600

    Update path in run_bitbake in accordance to new project structure

--- run_bitbake.sh
+++ run_bitbake.sh
@@ -5,7 +5,7 @@ while (( "$#" )); do
 done
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo "Launching bitbake $args"
-cd $DIR/..
+cd $DIR/../poky
 . ./oe-init-build-env
 bitbake $args  | sed -u 's@| @@'
 exit 0
