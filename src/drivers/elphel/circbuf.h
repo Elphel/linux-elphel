0bca6d230cfc076df15c930de583419a808c48cf
Mikhail Karpenko <mikhail@elphel.com>
2016-07-20 21:50:03 -0600

    Update doxygen documentation for cirbuf

--- src/drivers/elphel/circbuf.h
+++ src/drivers/elphel/circbuf.h
@@ -1,43 +1,52 @@
-// FILE NAME  : cxsdma.h
-// read/write image and FPN buffers from SDRAM
+/** @file circbuf.h
+ *
+ * @brief Drivers to manipulate large circular buffer that holds compressed
+ * images/video. Buffer frame data is filled in by the FPGA, frame pointers and
+ * essential frames metadata filled during servicing of the interrupts.
+ *
+ * @copyright Copyright (C) 2016 Elphel, Inc
+ *
+ * @par <b>License</b>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #ifndef _CIRCBUF_H
 #define _CIRCBUF_H
 
 #include <linux/poll.h>
 
-int          circbuf_all_open  (struct inode *inode,  struct file *filp); // set filesize
+/** @brief Circular buffer private data */
+struct circbuf_priv_t {
+	int                 minor;                             ///< device file minor number
+	unsigned long       *buf_ptr;                          ///< pointer to circular buffer memory region
+	dma_addr_t          phys_addr;                         ///< physical address of memory region reported by memory driver
+};
+extern struct circbuf_priv_t *circbuf_priv_ptr;
+extern wait_queue_head_t circbuf_wait_queue;
+
+int          circbuf_all_open  (struct inode *inode,  struct file *filp); // set file size
 int          circbuf_all_release(struct inode *inode, struct file *filp);
 loff_t       circbuf_all_lseek (struct file * file,   loff_t offset, int orig);
 ssize_t      circbuf_all_write (struct file * file,   const char * buf, size_t count, loff_t *off);
 ssize_t      circbuf_all_read  (struct file * file,   char * buf, size_t count, loff_t *off);
 int          circbuf_all_mmap  (struct file *file,    struct vm_area_struct *vma);
 unsigned int circbuf_all_poll  (struct file *file,    poll_table *wait);
-//!just to notify it is not implemented
-int          circbuf_all_ioctl(struct inode *inode, struct file *filp, unsigned int cmd, unsigned long arg);
 
-
-
-int          circbuf_open  (struct inode *inode, struct file *filp); // set filesize
+int          circbuf_open  (struct inode *inode, struct file *filp);      // set file size
 loff_t       circbuf_lseek (struct file * file, loff_t offset, int orig);
 ssize_t      circbuf_write (struct file * file, const char * buf, size_t count, loff_t *off);
 ssize_t      circbuf_read  (struct file * file, char * buf, size_t count, loff_t *off);
 int          circbuf_mmap  (struct file *file, struct vm_area_struct *vma);
 unsigned int circbuf_poll  (struct file *file,    poll_table *wait);
-
-//int init_ccam_dma_buf_ptr(void);
-/*!======================================================================================
- *! Wait queue for the processes waiting for a new frame to appear in the circular buffer
- *!======================================================================================*/
-extern wait_queue_head_t circbuf_wait_queue;
-
-// private data
-struct circbuf_priv_t {
-	int                 minor;
-	unsigned long       *buf_ptr;
-	dma_addr_t          phys_addr;
-};
-extern struct circbuf_priv_t *circbuf_priv_ptr;
 
 /* debug code follows */
 extern unsigned short circbuf_quality;
