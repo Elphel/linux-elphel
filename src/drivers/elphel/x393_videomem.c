8b081d6b6ec2b11238f52295b13858c9d512fbcc
Oleg Dzhimiev <oleg@elphel.com>
2018-01-12 11:04:26 -0700

    turned off debug

--- src/drivers/elphel/x393_videomem.c
+++ src/drivers/elphel/x393_videomem.c
@@ -14,7 +14,7 @@
 *  You should have received a copy of the GNU General Public License
 *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 *******************************************************************************/
-#define DEBUG
+//#define DEBUG
 #include <linux/interrupt.h>
 #include <linux/platform_device.h>
 #include <linux/of_device.h>
